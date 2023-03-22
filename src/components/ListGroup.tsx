7f830d7cc88329d0cd0035a8a6698365af6ac30d
ReDay Zarra <redayzarra@gmail.com>
2023-03-21 21:47:48 -0400

    Adding list items dynamically

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,12 +1,15 @@
 function ListGroup() {
+  const items = ["New York", "San Fransico", "London", "Paris", "Tokyo"];
+
   return (
-    <ul className="list-group">
-      <li className="list-group-item">Cras justo odio</li>
-      <li className="list-group-item">Dapibus ac facilisis in</li>
-      <li className="list-group-item">Morbi leo risus</li>
-      <li className="list-group-item">Porta ac consectetur ac</li>
-      <li className="list-group-item">Vestibulum at eros</li>
-    </ul>
+    <>
+      <h1>List</h1>
+      <ul className="list-group">
+        {items.map((item) => (
+          <li key = {item}>{item}</li>
+        ))}
+      </ul>
+    </>
   );
 }
 
