feb0d1d0fcaab7d20b9931c8f505c466db6ebf77
ReDay Zarra <redayzarra@gmail.com>
2023-03-28 23:44:37 -0400

    Starting lesson on events

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,7 +1,5 @@
 function ListGroup() {
   let items = ["New York", "San Fransico", "London", "Paris", "Tokyo"];
-  items = [];
-
 
   return (
     <>
@@ -9,7 +7,13 @@ function ListGroup() {
       {items.length == 0 && <p>No item found</p>}
       <ul className="list-group">
         {items.map((item) => (
-          <li key={item}>{item}</li>
+          <li
+            className="list-group-item"
+            key={item}
+            onClick={() => console.log("Clicked")}
+          >
+            {item}
+          </li>
         ))}
       </ul>
     </>
