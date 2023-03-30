8dcd1b7ed1b9d92b5cea589dce2c32e43bbe0b4f
ReDay Zarra <redayzarra@gmail.com>
2023-03-29 23:49:20 -0400

    Understanding parameter error messages in TS

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,16 +1,17 @@
 function ListGroup() {
   let items = ["New York", "San Fransico", "London", "Paris", "Tokyo"];
 
+  const handleClick = (event) => console.log(event);
   return (
     <>
       <h1>List</h1>
       {items.length == 0 && <p>No item found</p>}
       <ul className="list-group">
-        {items.map((item) => (
+        {items.map((item, index) => (
           <li
             className="list-group-item"
             key={item}
-            onClick={() => console.log("Clicked")}
+            onClick={(event) => console.log(event)}
           >
             {item}
           </li>
