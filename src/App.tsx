f36bc2c8409cb5c676408f2ab521db7a59a8a019
ReDay Zarra <redayzarra@gmail.com>
2023-04-01 12:05:21 -0400

    Implementing Bootstrap button

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import Alert from "./components/Alert";
+import Button from "./components/Button";
 
 function App() {
   return (
     <div>
-      <Alert>
+      <Button>
         Hello <span>World</span>
-      </Alert>
+      </Button>
     </div>
   );
 }
---
+++ src/components/Button.tsx
@@ -0,0 +1,11 @@
+import React, { ReactNode } from "react";
+
+interface Props {
+  children: ReactNode;
+}
+
+const Button = ({ children }: Props) => {
+  return <div className="btn btn-primary">{children}</div>;
+};
+
+export default Button;
