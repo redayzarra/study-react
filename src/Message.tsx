c37ee00b8d21655923ef514d5aa5c3ff775a56d2
ReDay Zarra <redayzarra@gmail.com>
2023-04-01 11:17:10 -0400

    Reformatting and understanding props

--- src/Message.tsx
+++ src/Message.tsx
@@ -1,11 +1,9 @@
 // PascalCasing - best practice
 function Message() {
-
   // JSX: Javascript XML
   const name = "Ashley";
-  if (name)
-    return <h1>I love {name}</h1>;
-  return <h1>I love you</h1>
+  if (name) return <h1>I love {name}</h1>;
+  return <h1>I love you</h1>;
 }
 
 export default Message;
--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,14 +1,18 @@
 import { useState } from "react";
 
-function ListGroup() {
-  let items = ["New York", "San Fransico", "London", "Paris", "Tokyo"];
+// items : [], heading:string
+interface Props {
+  items: string[];
+  heading: string;
+}
 
+function ListGroup({ items, heading }: Props) {
   // Hook
   const [selectedIndex, setSelectedIndex] = useState(-1);
 
   return (
     <>
-      <h1>List</h1>
+      <h1>{heading}</h1>
       {items.length == 0 && <p>No item found</p>}
       <ul className="list-group">
         {items.map((item, index) => (
