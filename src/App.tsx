7163c5eceaa4436cc72f1af8cd2c1658d86e642b
ReDay Zarra <redayzarra@gmail.com>
2023-04-01 11:40:16 -0400

    Removing ListGroup and starting Alert

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,9 @@
-import ListGroup from "./components/ListGroup";
+import Alert from "./components/Alert";
 
 function App() {
-  let items = ["New York", "San Fransico", "London", "Paris", "Tokyo"];
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  }
-
   return (
     <div>
-      <ListGroup items={items} heading={"Cities"} onSelectItem = {handleSelectItem}/>
+      <Alert />
     </div>
   );
 }
