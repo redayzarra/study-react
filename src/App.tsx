95578dec6234826e75d71db727edf7b5ba3bdfe9
ReDay Zarra <redayzarra@gmail.com>
2023-04-01 11:55:31 -0400

    Passing children to props

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,9 @@ import Alert from "./components/Alert";
 function App() {
   return (
     <div>
-      <Alert />
+      <Alert>
+        Hello <span>World</span>
+      </Alert>
     </div>
   );
 }
