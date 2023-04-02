9de507d4cf8365b26eacc57804b426664edf75fd
ReDay Zarra <redayzarra@gmail.com>
2023-04-02 07:24:12 -0400

    Implementing dismissable alerts on button click

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
+import { useState } from "react";
+import Alert from "./components/Alert";
 import Button from "./components/Button";
 
 function App() {
+  const [alertVisible, setAlertVisibility] = useState(false);
+
   return (
     <div>
-      <Button color="danger" onClick={() => console.log("Nice!")}>
+      {alertVisible && (
+        <Alert onClose={() => setAlertVisibility(false)}>My Alert</Alert>
+      )}
+      <Button color="danger" onClick={() => setAlertVisibility(true)}>
         Hello <span> World</span>
       </Button>
     </div>
