863fe9052922cd78af7c69dc84db57b0f93e4eb8
ReDay Zarra <redayzarra@gmail.com>
2023-04-03 08:16:19 -0400

    Understanding how objects are copied and modified

--- src/App.tsx
+++ src/App.tsx
@@ -7,21 +7,16 @@ import Like from "./components/Like";
 import Message from "./components/Message";
 
 function App() {
-  const [tags, setTags] = useState(["happy", "cheerful"]);
+  const [bugs, setBugs] = useState([
+    { id: 1, title: "Bug 1", fixed: false },
+    { id: 2, title: "Bug 2", fixed: false },
+  ]);
 
   const handleClick = () => {
-    // Add new item
-    setTags([...tags, "exciting"]);
-
-    //remove
-    setTags(tags.filter((tag) => tag !== "happy"));
-
-    //update
-    setTags(tags.map((tag) => (tag == "happy" ? "happiness" : tag)));
+    setBugs(bugs.map((bug) => (bug.id == 1 ? { ...bug, fixed: true } : bug)));
   };
   return (
     <div>
-      {tags}
       <button onClick={handleClick}>Button</button>
     </div>
   );
