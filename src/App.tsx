4541ef3d0cd35f0b169622b3783e409f5dee940e
ReDay Zarra <redayzarra@gmail.com>
2023-04-03 08:37:41 -0400

    Adding navbar

--- src/App.tsx
+++ src/App.tsx
@@ -8,31 +8,9 @@ import Message from "./components/Message";
 import produce from "immer";
 
 function App() {
-  const [bugs, setBugs] = useState([
-    { id: 1, title: "Bug 1", fixed: false },
-    { id: 2, title: "Bug 2", fixed: false },
-  ]);
+  const [cartItems, setCartItems] = useState(["Product1", "Product2"]);
 
-  const handleClick = () => {
-    // setBugs(bugs.map((bug) => (bug.id == 1 ? { ...bug, fixed: true } : bug)));
-
-    setBugs(
-      produce((draft) => {
-        const bug = draft.find((bug) => bug.id == 1);
-        if (bug) bug.fixed = true;
-      })
-    );
-  };
-  return (
-    <div>
-      {bugs.map((bug) => (
-        <p key={bug.id}>
-          {bug.title} {bug.fixed ? "Fixed" : "New"}
-        </p>
-      ))}
-      <button onClick={handleClick}>Button</button>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
