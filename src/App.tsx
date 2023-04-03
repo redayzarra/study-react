cb496e1b3a9b335e4606307ae92f629f265e3c52
ReDay Zarra <redayzarra@gmail.com>
2023-04-03 08:31:25 -0400

    Learning Immer

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import ListGroup from "./components/ListGroup";
 import { BsFillCalendarFill } from "react-icons/bs";
 import Like from "./components/Like";
 import Message from "./components/Message";
+import produce from "immer";
 
 function App() {
   const [bugs, setBugs] = useState([
@@ -13,10 +14,22 @@ function App() {
   ]);
 
   const handleClick = () => {
-    setBugs(bugs.map((bug) => (bug.id == 1 ? { ...bug, fixed: true } : bug)));
+    // setBugs(bugs.map((bug) => (bug.id == 1 ? { ...bug, fixed: true } : bug)));
+
+    setBugs(
+      produce((draft) => {
+        const bug = draft.find((bug) => bug.id == 1);
+        if (bug) bug.fixed = true;
+      })
+    );
   };
   return (
     <div>
+      {bugs.map((bug) => (
+        <p key={bug.id}>
+          {bug.title} {bug.fixed ? "Fixed" : "New"}
+        </p>
+      ))}
       <button onClick={handleClick}>Button</button>
     </div>
   );
