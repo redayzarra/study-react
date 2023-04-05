d88dd4516d7056d10b611953a290f284e5740f40
ReDay Zarra <redayzarra@gmail.com>
2023-04-05 09:23:33 -0400

    Implementing name form

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,6 @@
 import { useState } from "react";
-import Alert from "./components/Alert";
-import Button from "./components/Button";
-import ListGroup from "./components/ListGroup";
-import { BsFillCalendarFill } from "react-icons/bs";
-import Like from "./components/Like";
-import Message from "./components/Message";
 import produce from "immer";
-import Navbar from "./components/Navbar";
-import Cart from "./components/Cart";
-import ExpandableText from "./components/ExpandableText";
+import Form from "./components/Form";
 
 function App() {
   const [cart, setCart] = useState({
@@ -30,19 +22,7 @@ function App() {
 
   return (
     <div>
-      <ExpandableText maxChars={100}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque nemo ad
-        commodi, sunt ab cupiditate cumque mollitia magnam tenetur consequuntur
-        id molestiae nam omnis veritatis voluptatem? Earum velit quibusdam illum
-        sit mollitia soluta veritatis doloribus, numquam recusandae ad, nostrum
-        blanditiis aperiam consequatur modi debitis. Rerum id facere eaque
-        quaerat omnis laboriosam expedita earum dolorum, eius aut quos. Nobis
-        voluptatem eveniet earum, ea culpa sunt cum. Dolor, accusantium alias.
-        Mollitia tenetur reprehenderit possimus deleniti aspernatur nam
-        cupiditate ipsa consequuntur debitis? Deleniti ea minima dolorum,
-        consectetur quidem voluptatum iste nam, ex officia praesentium animi
-        laboriosam illum nobis error culpa velit eveniet? Qui.
-      </ExpandableText>
+      <Form></Form>
     </div>
   );
 }
