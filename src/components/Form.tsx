ff385f9b8597682310e058ff61802e220f67390e
ReDay Zarra <redayzarra@gmail.com>
2023-04-05 09:29:46 -0400

    Implementing submit button

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -9,12 +9,17 @@ const Form = () => {
         </label>
         <input id="name" type="text" className="form-control" />
       </div>
+
       <div className="mb-3">
         <label htmlFor="" className="form-label">
           Age
         </label>
         <input id="age" type="number" className="form-control" />
       </div>
+
+      <button className="btn btn-primary" type="submit">
+        Submit
+      </button>
     </form>
   );
 };
