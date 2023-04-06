304c0e8e6bea6f9b8ae8b00953f3f72a782ecc33
ReDay Zarra <redayzarra@gmail.com>
2023-04-06 11:55:28 -0400

    Implementing expense tracker list with table

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,25 @@
 import { useState } from "react";
 import produce from "immer";
 import Form from "./components/Form";
+import ExpenseList from "./expense-tracker/components/ExpenseList";
 
 function App() {
-  const [cart, setCart] = useState({})
+  const [expenses, setExpenses] = useState([
+    { id: 1, description: "aaa", amount: 10, category: "Utilities" },
+    { id: 2, description: "bbb", amount: 10, category: "Utilities" },
+    { id: 3, description: "ase", amount: 10, category: "Utilities" },
+    { id: 4, description: "aw3", amount: 10, category: "Utilities" },
+    { id: 5, description: "adf", amount: 10, category: "Utilities" },
+  ]);
 
-  const handleClick = () => {}
+  const handleClick = () => {};
 
   return (
     <div>
-      <Form></Form>
+      <ExpenseList
+        expenses={expenses}
+        onDelete={(id) => setExpenses(expenses.filter((e) => e.id != id))}
+      ></ExpenseList>
     </div>
   );
 }
