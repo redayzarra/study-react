0c74741dc5fc1ef8d4672e5ec53707d56f7be7bc
ReDay Zarra <redayzarra@gmail.com>
2023-04-07 08:13:08 -0400

    Implementing item filtering

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,32 @@ import { useState } from "react";
 import produce from "immer";
 import Form from "./components/Form";
 import ExpenseList from "./expense-tracker/components/ExpenseList";
+import ExpenseFilter from "./expense-tracker/components/ExpenseFilter";
 
 function App() {
+  const [selectedCategory, setSelectedCategory] = useState("");
+
   const [expenses, setExpenses] = useState([
     { id: 1, description: "aaa", amount: 10, category: "Utilities" },
     { id: 2, description: "bbb", amount: 10, category: "Utilities" },
-    { id: 3, description: "ase", amount: 10, category: "Utilities" },
-    { id: 4, description: "aw3", amount: 10, category: "Utilities" },
+    { id: 3, description: "ase", amount: 10, category: "Groceries" },
+    { id: 4, description: "aw3", amount: 10, category: "Entertainment" },
     { id: 5, description: "adf", amount: 10, category: "Utilities" },
   ]);
 
-  const handleClick = () => {};
+  const visibleExpenses = selectedCategory
+    ? expenses.filter((e) => e.category == selectedCategory)
+    : expenses;
 
   return (
     <div>
+      <div className="mb-3">
+        <ExpenseFilter
+          onSelectCategory={(category) => setSelectedCategory(category)}
+        ></ExpenseFilter>
+      </div>
       <ExpenseList
-        expenses={expenses}
+        expenses={visibleExpenses}
         onDelete={(id) => setExpenses(expenses.filter((e) => e.id != id))}
       ></ExpenseList>
     </div>
