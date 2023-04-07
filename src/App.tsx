0f9bdfe8b06fbac8c66846ff48fd9bd6ac68a68d
ReDay Zarra <redayzarra@gmail.com>
2023-04-07 08:27:37 -0400

    Building input form for expenses

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import produce from "immer";
 import Form from "./components/Form";
 import ExpenseList from "./expense-tracker/components/ExpenseList";
 import ExpenseFilter from "./expense-tracker/components/ExpenseFilter";
+import ExpenseForm from "./expense-tracker/ExpenseForm";
+
+export const categories = ["Groceries", "Utilities", "Entertainment"];
 
 function App() {
   const [selectedCategory, setSelectedCategory] = useState("");
@@ -21,6 +24,9 @@ function App() {
 
   return (
     <div>
+      <div className="mb-5">
+        <ExpenseForm></ExpenseForm>
+      </div>
       <div className="mb-3">
         <ExpenseFilter
           onSelectCategory={(category) => setSelectedCategory(category)}
