9fec9544a3b22f44b78b17da60f2486e8ef9969c
ReDay Zarra <redayzarra@gmail.com>
2023-04-07 08:29:12 -0400

    Moving expense form to correct location

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import produce from "immer";
 import Form from "./components/Form";
 import ExpenseList from "./expense-tracker/components/ExpenseList";
 import ExpenseFilter from "./expense-tracker/components/ExpenseFilter";
-import ExpenseForm from "./expense-tracker/ExpenseForm";
+import ExpenseForm from "./expense-tracker/components/ExpenseForm";
 
 export const categories = ["Groceries", "Utilities", "Entertainment"];
 
--- src/expense-tracker/components/ExpenseForm.tsx
+++ src/expense-tracker/components/ExpenseForm.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { categories } from "../App";
+import { categories } from "../../App";
 
 const ExpenseForm = () => {
   return (
