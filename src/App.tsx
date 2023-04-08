9e9f058b607fae631540b8e6db498d82e1e7ac1a
ReDay Zarra <redayzarra@gmail.com>
2023-04-08 09:55:20 -0400

    Understanding how to fetch data with Axios

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,27 @@ import { useEffect, useRef, useState } from "react";
 import produce from "immer";
 import axios from "axios";
 
-function App() {
-  const [users, setUsers] = useState([]);
-  
-  useEffect(() => {
-    axios.get("https://jsonplaceholder.typicode.com/users")
-  })
+interface User {
+  id: number;
+  name: string;
+}
 
-  return <div></div>;
+function App() {
+  const [users, setUsers] = useState<User[]>([]);
+
+  useEffect(() => {
+    axios
+      .get<User[]>("https://jsonplaceholder.typicode.com/users")
+      .then((res) => setUsers(res.data));
+  }, []);
+
+  return (
+    <ul>
+      {users.map((user) => (
+        <li key={user.id}>{user.name}</li>
+      ))}
+    </ul>
+  );
 }
 
 export default App;
