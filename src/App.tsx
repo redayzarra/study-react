afc2a1d9232557d1e398f372ec6fa281bba86a2e
ReDay Zarra <redayzarra@gmail.com>
2023-04-08 09:33:57 -0400

    Understanding effect cleanup and unmounting

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,18 @@
 import { useEffect, useRef, useState } from "react";
 import produce from "immer";
-import ProductList from "./components/ProductList";
+
+const connect = () => console.log("Connecting");
+const disconnect = () => console.log("Disconnecting");
 
 function App() {
-  const [category, setCategory] = useState("");
-
   useEffect(() => {
     document.title = "My App";
+    connect();
+
+    return () => disconnect();
   });
 
-  return (
-    <div>
-      <select
-        id=""
-        className="form-select"
-        onChange={(event) => setCategory(event.target.value)}
-      >
-        <option value=""></option>
-        <option value="Clothing">Clothing</option>
-        <option value="Household">Household</option>
-      </select>
-      <ProductList category={category}></ProductList>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
